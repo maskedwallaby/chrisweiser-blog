f84d257f7ffff0f71054467f046765d16243025c
Chris Weiser <maskedwallaby@pm.me>
2021-06-19 16:39:10 -0500

    Unset eleventy _site base dir.

--- .eleventy.js
+++ .eleventy.js
@@ -117,7 +117,7 @@ module.exports = function(eleventyConfig) {
     // You can also pass this in on the command line using `--pathprefix`
 
     // Optional (default is shown)
-    pathPrefix: "_site",
+    pathPrefix: "/",
     // -----------------------------------------------------------------
 
     // Pre-process *.md files with: (default: `liquid`)
